3a8f2eef3e32b52c14afa0452f776cd0a9556eab
karcot1 <111666655+karcot1@users.noreply.github.com>
2023-10-31 14:45:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -104,9 +104,10 @@ SELECT
 FROM dataform.ad_and_ingest_metadata
 WHERE STATUS = "SUCCESS" AND EXTRACT(DATE FROM LOAD_DATE) = EXTRACT(DATE FROM CURRENT_TIMESTAMP())
     """
-    results = client.query(query)
-    print(results)
-    # assessment = [[row[i] for row in results] for i in range(len(results[0]))][1]
+    results = list(client.query(query))
+
+    assessment = [[row[i] for row in results] for i in range(len(results[0]))][1]
+    print(assessment)
     return assessment
  
 
